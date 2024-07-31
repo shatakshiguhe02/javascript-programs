54d64d7444653ba7cc4988aa1b79eb61fd21ed76
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-07-31 07:53:33 +0530

    javascript variables
    javascript variables

---
+++ 01-variableassignment.js
@@ -0,0 +1,7 @@
+
+
+console.log("My city name is : Amravati ")
+console.log("My best friend name is : Rohini ");
+console.log("My coutry name is : India");
+console.log("My school name is : Mani bai gujarati High School");
+console.log("My college name is : PRPCEM ");
