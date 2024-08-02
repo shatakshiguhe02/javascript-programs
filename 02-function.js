0f5dac3e7bb49d7ce440e13b8140a68ac95e0d97
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-08-02 08:29:57 +0530

    function topic
    function topic

---
+++ 02-function.js
@@ -0,0 +1,41 @@
+console.log("=============function with no arguments and no return value==========");
+
+
+function show() {
+    console.log("Inside show function");
+}
+show();
+
+
+console.log("=============function with arguments and no return value==========");
+
+
+function multipy(n1, n2) {
+    console.log("Given Numbers:", n1, n2);
+    var result = n1 * n2;
+    console.log("Multiplication is", result);
+}
+multipy(8, 9);
+multipy(80, 90);
+multipy(8.1, 9.1);
+multipy(8);
+
+console.log("=============function with no arguments and return value==========");
+
+
+function demo() {
+    console.log("Inside demo function....");
+    return "Good Morning"
+}
+var result = demo();
+console.log(result);
+
+console.log("=============function with arguments and return value==========");
+
+function square(num) {
+    console.log("Given number of square is :", num);
+    var result = num*num;
+    return result;
+}
+var returnValue = square(5);
+console.log("Square is",returnValue);
