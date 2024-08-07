5b97a9d3acc18abb8e616e682e8fb5d0269d1a6f
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-08-07 21:31:01 +0530

    Update 05-operatorAssigC.js

--- 05-operatorAssigC.js
+++ 05-operatorAssigC.js
@@ -1,8 +1,9 @@
 
 function eligibilityCheck(gradScore, hscScore, sscScore, candidateName) {
-    console.log(`Given criteria's are: ${gradScore}, ${hscScore}, ${sscScore}, ${candidateName}`);
-    var result = gradScore >= 70 || hscScore >= 80 || sscScore > 90 ? `Congrats ${candidateName} you are eligible for TSC interview` : `unfortunately ${candidateName} you are not eligible for TSC interview`;
+    console.log(`1. Given criteria's are: ${gradScore}, ${hscScore}, ${sscScore}, ${candidateName}`);
+    var result = gradScore >= 70 || hscScore >= 80 || sscScore > 90 ? `Congrats ${candidateName}you are eligible for TSC interview` : `unfortunately ${candidateName}you are not eligible for TSC interview`;
     console.log(result);
+    console.log("----------------------------------------------------");
 }
 eligibilityCheck(80, 86, 90, "Shatakshi ");
 eligibilityCheck(70, 65, 55, "Priya ");
