eec9fbd241b842ffb57b7ac6627e9d41ece94e76
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-08-07 21:17:49 +0530

    operator-assignment
    operator-assignment

---
+++ 05-operatorA.js
@@ -0,0 +1,36 @@
+
+function squareOfWordLength(sentence) {
+    console.log(`1. Given sentence is : ${sentence}`);
+    var length = sentence.length;
+    console.log(`1.1 length of the sentence : ${length}`);
+    var square = length * length;
+    return square;  
+}
+var result = squareOfWordLength("JavaScript");
+console.log(`1.2 Square of word length is : ${ result}`);
+console.log("------------------------------------------------");
+var result = squareOfWordLength("Google Chrome");
+console.log(`1.2 Square of word length is : ${ result}`);
+console.log("------------------------------------------------");
+var result = squareOfWordLength("Web Developer Smart");
+console.log(`1.2 Square of word length is : ${ result}`);
+console.log("================================================");
+
+function divideMultiply( ) {
+    var string = "I am Angular Developer";
+    console.log(`2. Given string is : ${string}`);
+    var stringLength = string.length;
+    console.log(`2.1 Length of the given string : ${stringLength}`);
+    var result = string.split(" ");
+    var totalWords = result.length;
+    console.log(`2.2 Length of words available  : ${totalWords}`);
+
+    console.log("------------------------------------------------");
+    var divisionResult = stringLength/totalWords;
+    console.log(`2.3 String length divided by string words : ${divisionResult}`);
+
+    console.log("------------------------------------------------");
+    var multiplyResult = stringLength*totalWords;
+    console.log(`2.4 String length multiply by string words : ${multiplyResult}`);
+}
+divideMultiply();
---
+++ 05-operatorB.js
@@ -0,0 +1,36 @@
+function greaterNumber(num1,num2) {
+    var result = num1>=num2 ? num1 : num2 ;
+    console.log(`1. Given numbers are : ${num1},${num2} Greater number is : ${result}`);
+     
+}
+greaterNumber(10,-10);
+greaterNumber(800,899);
+
+console.log("================================");
+
+function isEvenOrOddNum(number){
+    var result = number%2==0 ? true : false ;
+    console.log(`2. Given number ${number} is even : ${result}`);
+}
+isEvenOrOddNum(29);
+isEvenOrOddNum(44);
+isEvenOrOddNum(0);
+isEvenOrOddNum(101);
+
+console.log("=================================");
+
+function wordLength(word) {
+    console.log(`3. Given string is : ${word}`);
+    var result = word.length;
+    console.log(`3.1 Given length of word is : ${result}`);
+    return res = word.length%2==0 ? 'EVEN' : 'ODD';
+}
+var res = wordLength("JavaScript");
+console.log(`3.2 Value of given string is : ${res}`);
+console.log("---------------------------------");
+var res = wordLength("developer");
+console.log(`3.3 Value of given string is : ${res}`);
+console.log("---------------------------------");
+var res = wordLength("Google");
+console.log(`3.4 Value of given string is : ${res}`);
+
