9ba918e6c4f69b0acb21c047aa5d74103b737de0
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-08-10 19:47:59 +0530

    for assig - 01
    forloop assignment - 01

---
+++ 06-for-Assignment01.js
@@ -0,0 +1,35 @@
+
+console.log(`======= Print the numbers from 5 to 15 ==========`);
+for (let i = 5; i <= 15; i++) {
+    console.log(i);
+}
+
+console.log(`======= Print the numbers from 50 to 40 ==========`);
+for (let i = 50; i >= 40; i--) {
+    console.log(i);
+}
+
+console.log(`======= Print the First 15 odd numbers ==========`);
+for (let i = 1; i <= 30; i = i + 2) {
+    console.log(i);
+}
+
+console.log(`======= Print the First 10 even numbers ==========`);
+for (let i = 0; i <= 20; i=i+2) {
+    console.log(i);
+}
+
+console.log(`======= Print the Table of 5 ==========`);
+for (let i = 5; i <= 50; i = i + 5) {
+    console.log(i);
+}
+
+console.log(`======= Print the Table of 10 ==========`);
+for (let i = 10; i <= 100; i = i + 10) {
+    console.log(i);
+}
+
+console.log(`======= Print the Table of 10 in reverse order==========`);
+for (let i = 100; i >= 10; i = i - 10) {
+    console.log(i);
+}
