3183f2770b445b92babdc1971c34e51f626fcd92
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-08-12 22:07:33 +0530

    06-loop-assignment
    loop-assignment

---
+++ 06-loop-assig-a.js
@@ -0,0 +1,41 @@
+console.log("======================Vowel count================================");
+
+var sentence = "I am very good IT Developer";
+vowels = "aeiou";
+vowelCount = 0;
+for (let i = 0; i < sentence.length; i++) {
+    var character = sentence.charAt(i).toLowerCase();
+    if (vowels.includes(character)) {
+        vowelCount++;
+    }
+}
+console.log(`Total number of vowels including small and capital are : ${vowelCount}`)
+
+console.log("======================Sum of cube================================");
+
+// sum of 1 to 5;
+// var sum = 0;
+// for (let index = 1; index <=5; index++) {
+//     sum = sum + index;
+// }
+// console.log(`sum of 1 to 5: ${sum}`);
+var sumCube = 0;
+for (let index = 1; index <= 5; index++) {
+    sumCube = sumCube + index * index * index;
+}
+console.log(`Sum of the cube of numbers 1 to 5 : ${sumCube}`);
+
+
+console.log("================Odd-Position char from given string==============");
+
+function oddPositioned(string) {
+    var res = '';
+    for (let i = 0; i < string.length; i++) {
+        if ((i % 2 == 1) && string[i] != ' ') {
+            res += string[i];
+        }
+    }
+    console.log(res);
+}
+oddPositioned("Hard work always pays back");
+oddPositioned("Soon I will be UI IT Champ");
