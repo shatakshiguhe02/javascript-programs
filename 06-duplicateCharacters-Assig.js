82f061ac7b07e9b8f3f6a72edef7fbf0ed530815
shatakshiguhe02 <shatakshiguhe@gmail.com>
2024-08-28 17:59:02 +0530

    duplicate character using for loop
    To find duplicate character using for loop

--- 06-duplicateCharacters-Assig.js
+++ 06-duplicateCharacters-Assig.js
@@ -1,23 +1,49 @@
 
 console.log(`========Program to find duplicate characters=========`);
-console.log('');   
+// console.log('');   
 
-function duplicateCharCount(str){
-   const charCount = {};
-   for (const character of str) { //loop for each character in inputstring
-     if (character!==' ') {
-        charCount[character] = (charCount[character] || 0) + 1 ;
-     }
-   }
-   for (const character in charCount) {
-    if (charCount[character] > 1) {
-        console.log(`${character} - ${charCount[character]}`);
-     }
-   }
+// function duplicateCharCount(str){
+//    const charCount = {};
+//    for (const character of str) { //loop for each character in inputstring
+//      if (character!==' ') {
+//         charCount[character] = (charCount[character] || 0) + 1 ;
+//      }
+//    }
+//    for (const character in charCount) {
+//     if (charCount[character] > 1) {
+//         console.log(`${character} - ${charCount[character]}`);
+//      }
+//    }
+// }
+// const string = ["Banana", "Apple", "Software Developer"];
+
+// string.forEach(str => {
+//     console.log(`Duplicate characters in "${str}"`);
+//     duplicateCharCount(str);
+// });
+
+
+
+// ==========================
+let str = "Banana";  // a - 3, n - 2
+let finalOutput = {};
+for (let index = 0; index < str.length; index++) {
+    const element = str.charAt(index);
+    let count = 0;
+    for (let j = 0; j < str.length; j++) {
+        const char = str.charAt(j);
+        if (char==element) {
+            count++;     
+        }
+    }
+    // console.log(element, count);
+    if (count>1) {
+        finalOutput[element] = count;
+    }  
 }
-const string = ["Banana", "Apple", "Software Developer"];
-
-string.forEach(str => {
-    console.log(`Duplicate characters in "${str}"`);
-    duplicateCharCount(str);
-});
+// console.log(finalOutput);
+for (const key in finalOutput) {
+    const element = finalOutput[key];
+    console.log(`${key} - ${element}`);
+    
+}
